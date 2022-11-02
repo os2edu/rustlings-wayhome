c721838a918acec7761cbac1678d011c374c4252
Young King <yanckin@gmail.com>
2022-11-02 14:33:32 +0000

    move-1

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -17,12 +17,12 @@ fn main() {
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+fn fill_vec(vec: &Vec<i32>) -> Vec<i32> {
     let mut vec = vec;
 
     vec.push(22);
     vec.push(44);
     vec.push(66);
 
-    vec
+    *vec
 }
