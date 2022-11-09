99c2ae4e6f2cfe2cf428af49a154e6bad556d805
Young King <yanckin@gmail.com>
2022-11-09 16:00:06 +0000

    conversions

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,10 +35,29 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.is_empty() {
+            return Person::default();
+        }
+        let split = s.split_once(',');
+        match split {
+            Some((name, age)) => {
+                if name.is_empty() {
+                    return Person::default();
+                }
+                match age.parse::<usize>() {
+                    Ok(age) => Person {
+                        name: name.to_string(),
+                        age,
+                    },
+                    Err(_) => Person::default(),
+                }
+            }
+            None => Person::default(),
+        }
+
     }
 }
 
